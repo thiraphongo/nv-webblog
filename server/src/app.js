f81b13a2a7a8bb3e7b3b9d7a2a70a4f0afe58cba
duennhaw <daoza2009@gmail.com>
2020-07-07 16:58:11 +0700

    folder perpared

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,17 @@
-console.log('Hello')
+let express = require('express')
+const app = express()
+app.get('/status', function (req, res){
+    res.send('Hello nodejs server')
+})
+let port = 8081
+app.listen(port, function (){
+    console.log('server running on ' + port)
+})
+// get user by id
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน')
+})
+// get all user
+app.get('/users', function (req, res) {
+    res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
+})
