330356c7063d7cf2f61645f1f901528ab86d864b
thiraphongo <jackteerapong2017@gmail.com>
2020-08-13 12:04:31 +0700

    update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
-   
-    <router-view/>
-  </div>
+<div id="app">
+<back-header />
+<router-view/>
+</div>
 </template>
 
 <script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -10,6 +10,15 @@ import UserShow from '@/components/Users/ShowUser'
 // Authen
 import Login from '@/components/Login'
 
+// Comments
+import CommentIndex from '@/components/Comments/Index'
+
+//Blogs
+import BlogIndex from '@/components/Blogs/Index'
+import BlogCreate from '@/components/Blogs/CreateBlog'
+import BlogEdit from '@/components/Blogs/EditBlog'
+import BlogShow from '@/components/Blogs/ShowBlog'
+
 Vue.use(Router)
 
 export default new Router({
@@ -23,7 +32,7 @@ export default new Router({
       path: '/user/create',
       name: 'users-create',
       component: UserCreate
-    },
+    }, 
     {
       path: '/user/edit/:userId',
       name: 'user-edit',
@@ -40,5 +49,33 @@ export default new Router({
       name: 'login',
       component: Login
     },
-  ]
-})
+    // blogs
+    {
+      path: '/blogs',
+      name: 'blogs',
+      component: BlogIndex
+    },
+    {
+      path: '/blog/create',
+      name: 'blogs-edit',
+      component: BlogCreate
+    },
+    {
+      path: '/blog/edit/:blogId',
+      name: 'blog-edit',
+      component: BlogEdit
+     },
+     {
+      path: '/blog/:blogId',
+      name: 'blog',
+      component: BlogShow
+    },
+    // comments
+    {
+      path: '/comments',
+     name: 'comments',
+      component: CommentIndex
+    }]
+  })
+
+  
